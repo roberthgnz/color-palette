425074f5d1557898da8f5c3c6e6c64a4aaf9efdf
Eliut González <reliutg@gmail.com>
2021-08-04 23:12:13 +0200

    feat: show colors

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="container">
+    <h1>
+      COLORS
+      <span style="display: block"> Version 2.2.1 </span>
+    </h1>
+    <ul id="menu-colors">
+      <li v-for="(value, key) in colors" :key="key">
+        <a :href="`#${key}`"><span :class="`badge-${key}`"></span>{{ key }}</a>
+      </li>
+    </ul>
+    <h2></h2>
+    <div id="colors">
+      <div v-for="(value, key) in colors" :key="key">
+        <h3 class="color-title" :id="key">{{ key }}</h3>
+        <div class="color-group" :data-color="key">
+          <div
+            v-for="(color, index) in value"
+            :key="color"
+            class="color-c"
+            :title="`Copy color ${color}`"
+          >
+            <div
+              class="color-bg"
+              :id="color"
+              :style="{ backgroundColor: color }"
+            ></div>
+            <h4 class="color-name">{{ key }} {{ index + 1 }}</h4>
+            <span class="color-type">{{ color }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import colors from "./colors";
+export default {
+  setup() {
+    console.log(colors);
+    return { colors };
+  },
+};
+</script>
---
+++ src/components/FormatSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dropdown">
+    <button class="dropdown__btn">Copy Format: HEX - #AA1923</button>
+    <div class="dropdown__content">
+      <button class="dropdown__option" value="0">HEX - #AA1923</button>
+      <button class="dropdown__option" value="1">HEX - AA1923</button>
+      <button class="dropdown__option" value="2">RGB - (1, 2, 3)</button>
+      <button class="dropdown__option" value="3">RGBA - (1, 2, 3, 1.0)</button>
+      <button class="dropdown__option" value="4">HSL - (1, 2%, 3%)</button>
+      <button class="dropdown__option" value="5">
+        HSLA - (1, 2%, 3%, 1.0)
+      </button>
+    </div>
+  </div>
+</template>
